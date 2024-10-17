6888dc32bc84d165fc01d2f7ddc24805bdefc50a
Kara Jelley <karajelley@gmail.com>
2024-10-17 11:46:04 -0400

    Solved Lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,7 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    return Math.max(num1, num2);
+}
 
 
 
@@ -7,7 +9,20 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arrOfWords) {
+    let longestWord = "";
+
+    if (arrOfWords.length === 0) {
+        longestWord = null;
+    }
+
+    for (let word of arrOfWords) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
 
 
 
@@ -15,7 +30,13 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrToSum) {
+    let sumTotal = 0;
+    arrToSum.forEach((number) => {
+        sumTotal += number;
+    });
+    return sumTotal;
+}
 
 
 
@@ -23,7 +44,15 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arrToAvg) {
+    let avgTotal;
+    if (arrToAvg.length === 0) {
+        avgTotal = 0;
+    } else {
+        avgTotal = sumNumbers(arrToAvg)/arrToAvg.length
+    }
+    return avgTotal;
+}
 
 
 
@@ -31,4 +60,17 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords, wordToSearch) {
+    if (arrayOfWords.length === 0) {
+        return null
+    } else {
+        for (let word of arrayOfWords ) {
+            if (wordToSearch === word) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
